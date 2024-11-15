e178fe322a28c3634f2bb1e0193b0e7abad182f3
Luiz <luizeduardo2099@gmail.com>
2024-11-15 14:31:37 -0300

    .

---
+++ scr/Cliente.java
@@ -0,0 +1,63 @@
+public class Cliente {
+    private String nome;
+    private int idade;
+    private int id;
+    private List<Livro> livros;
+    private double valorTotal;
+
+    public Cliente(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.livros = new ArrayList<>();
+        this.valorTotal = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void alugarLivro(Livro livro){
+        this.livros.add(livro);
+        this.valorTotal += livro.getPreco();
+        Estoque.getEstoque().removerLivro(livro);
+    }
+
+    public void devolverLivro(Livro livro){
+      this.livros.remove(livro);
+      this.valorTotal -= livro.getPreco();
+      Estoque.getEstoque().adicionarLivro(livro);
+    }
+
+}
---
+++ scr/Estoque.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estoque {
+    private List<Livro> livros;
+
+    public Estoque() {
+        livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+    }
+
+    public void removerLivro(Livro livro) {
+        livros.remove(livro);
+    }
+
+    public Livro encontrarLivro(String titulo) {
+        for (Livro livro : livros) {
+            if (livro.getTitulo().equals(titulo)) {
+                return livro;
+            }
+        }
+        return null;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+}
